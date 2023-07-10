ce536b0e4369ab871c48efa3f949dd004272d64a
henzzo <henokgetachew500@gmail.com>
2023-07-10 15:14:48 +0300

    feat: added pagination for submissions

--- src/plugins/form-submissions/admin/src/pages/FormSubmissions/Components.tsx
+++ src/plugins/form-submissions/admin/src/pages/FormSubmissions/Components.tsx
@@ -23,6 +23,12 @@ export const Key = styled.span`
   font-size: 1.1rem;
 `
 
+export const PaginationWrapper = styled.div`
+  width: max-content;
+  margin-left: auto;
+  margin-top: 2rem;
+`
+
 export const Value = styled.span`
   display: inline-block;
   // chip shaped styling
@@ -30,5 +36,52 @@ export const Value = styled.span`
   border-radius: 0.5rem;
   background-color: #efefef;
   color: #000;
-
 `
+
+
+export const PageNumber = styled.button`
+  padding: .2rem .5rem;
+  border-radius: 4px;
+  border: 1px solid ${({theme}) => theme.colors.neutral200};
+  background-color: ${({theme}) => theme.colors.neutral0};
+  color: ${({theme}) => theme.colors.neutral800};
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 1.5rem;
+  margin: 0 .25rem;
+  cursor: pointer;
+  transition: all .2s ease-in-out;
+
+  &:hover {
+    background-color: ${({theme}) => theme.colors.neutral100};
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${({theme}) => theme.colors.primary100};
+  }
+
+  &:disabled {
+    opacity: .5;
+    cursor: not-allowed;
+  }
+
+  &.active {
+    background-color: ${({theme}) => theme.colors.primary100};
+    color: ${({theme}) => theme.colors.primary800};
+    border-color: ${({theme}) => theme.colors.primary200};
+  }
+
+  &.active:hover {
+    background-color: ${({theme}) => theme.colors.primary200};
+  }
+
+  &.active:focus {
+    box-shadow: 0 0 0 2px ${({theme}) => theme.colors.primary100};
+  }
+
+  &.active:disabled {
+    opacity: 1;
+    cursor: default;
+  }
+`;
--- src/plugins/form-submissions/admin/src/pages/FormSubmissions/index.tsx
+++ src/plugins/form-submissions/admin/src/pages/FormSubmissions/index.tsx
@@ -18,7 +18,9 @@ import {
   Layout,
   Link,
   LinkButton,
+  NextLink,
   Pagination,
+  PreviousLink,
   TextButton,
   Tooltip,
   Typography,
@@ -28,8 +30,8 @@ import {
 import {ArrowLeft, Calendar, File, Information, Pencil, User,} from "@strapi/icons";
 import {LoadingIndicatorPage} from "@strapi/helper-plugin";
 import {Submissions} from "../../api/submissions";
-import {useParams} from "react-router-dom";
-import {Content, Entry, Key, Value} from "./Components";
+import {NavLink, useParams} from "react-router-dom";
+import {Content, Entry, Key, PageNumber, PaginationWrapper, Value,} from "./Components";
 
 type FormSubmission = {
   id: string;
@@ -41,6 +43,19 @@ type FormSubmission = {
     formId: string;
     [key: string]: any;
   }[];
+};
+
+type Form = {
+  id: string;
+  title: string;
+  form: any[];
+};
+
+type Pagination = {
+  page: number;
+  pageCount: number;
+  pageSize: number;
+  total: number;
 };
 
 const AccordionAction = ({ submittedAt }: { submittedAt: string }) => (
@@ -69,8 +84,29 @@ const FileTypes = {
   video: "video",
 } as const;
 
+const getFileType = (mimeType: string) => {
+  switch (mimeType) {
+    case "application/pdf":
+      return FileTypes.pdf;
+    case "application/msword":
+    case "application/wps-office.docx":
+      return FileTypes.doc;
+    case "application/vnd.ms-excel":
+    case "application/wps-office.xlsx":
+      return FileTypes.xls;
+    case "image/png":
+    case "image/jpeg":
+    case "image/webp":
+      return FileTypes.image;
+    case "video/mp4":
+      return FileTypes.video;
+    default:
+      return FileTypes.doc;
+  }
+};
+
 const FormSubmissions = () => {
-  const [submissionsData, setSubmissionsData] = useState<FormSubmission>();
+  const [submissions, setSubmissions] = useState<FormSubmission>();
   const [files, setFiles] = useState<
     { name: string; url: string; mime: string }[]
   >([]);
@@ -79,95 +115,85 @@ const FormSubmissions = () => {
   const params = useParams<{ id: string }>();
   const [expandedID, setExpandedID] = useState(null);
 
+  const [page, setPage] = useState(1);
+  const [pagination, setPagination] = useState<Pagination>();
+
   const handleToggle = (id: any) => () => {
     setExpandedID((s) => (s === id ? null : id));
   };
 
-  const getFileType = (mimeType: string) => {
-    //write swich case for file types
-    switch (mimeType) {
-      case "application/pdf":
-        return FileTypes.pdf;
-      case "application/msword":
-      case "application/wps-office.docx":
-        return FileTypes.doc;
-      case "application/vnd.ms-excel":
-      case "application/wps-office.xlsx":
-        return FileTypes.xls;
-      case "image/png":
-      case "image/jpeg":
-        return FileTypes.image;
-      case "video/mp4":
-        return FileTypes.video;
-      default:
-        return FileTypes.doc;
-    }
-  };
+  useEffect(() => {
+    console.log("page : ", page);
 
-  const fetchData = async () => {
-    if (isLoading === false) setIsLoading(true);
-
-    // const formsWithSubmission = await todoRequests.getFormsWithSubmission();
-
-    const formsWithSubmission = await Submissions.get(
-      `/api/pages/${params?.id}`,
-      {
-        params: {
-          populate: {
-            submissions: {
-              populate: "*",
-            },
-            form: {
-              populate: ["label", "name", "items"],
-            },
-          },
-        },
-      }
-    )
-      .then<FormSubmission>((res: any) => {
-        if (res.status !== 200) throw new Error("Failed to fetch pages");
-
-        console.log("forms ----  : ", res.data.data);
+    const fetchSubmissions = async () => {
+      setIsLoading(true);
+      const selectedForm = await Submissions.get(
+        `/api/pages/${params?.id}`,
+        {}
+      ).then((res: any) => {
         const attributes = res.data.data.attributes;
-
-        const submissions: FormSubmission["submissions"] = (
-          attributes.submissions.data as any[]
-        ).map(({ id, attributes }: any) => ({
-          id,
-          submittedAt: attributes.createdAt,
-          formId: attributes.formId,
-          formData: attributes.formData,
-        }));
-
         return {
           id: res.data.data.id,
           title: attributes.title,
           form: attributes.form,
-          submissions,
         };
-      })
-      .catch((err) => {
-        console.log("err fetching pages --> : ", err);
       });
 
-    if (formsWithSubmission) {
-      setSubmissionsData(formsWithSubmission);
-    }
-    setIsLoading(false);
-  };
+      const submissions = await Submissions.get(
+        "/form-submissions/submissions",
+        {
+          params: {
+            sort: ["createdAt:desc"],
+            filters: {
+              formId: {
+                $eq: params?.id,
+              },
+            },
+            pagination: {
+              page: page,
+              pageSize: 5,
+              withCount: true,
+            },
+          },
+        }
+      ).then((res) => {
+        console.log("res.data.data ----> : ", res.data.meta.pagination);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+        const submissions = (res.data.data as any[]).map(
+          ({ id, attributes }: any) => ({
+            id,
+            submittedAt: attributes.createdAt,
+            formId: attributes.formId,
+            formData: attributes.formData,
+          })
+        );
+        // console.log("submission data : ", submissions);
+        return { submissions, pagination: res.data.meta.pagination };
+      });
 
+      setSubmissions({
+        ...selectedForm,
+        submissions: submissions.submissions,
+      });
+
+      setPagination(submissions.pagination);
+
+      setIsLoading(false);
+    };
+
+    fetchSubmissions();
+  }, [page]);
+
+  // fetch files uploaded
   useEffect(() => {
+    // todo : find a better way to relate the submission with the file
     Submissions.get("/api/upload/files")
       .then((res) => {
-        console.log(
-          "files res ----> : ",
-          res.data.map((file: any) => file)
-        );
-
+        /* console.log(
+           "files res ----> : ",
+           res.data.map((file: any) => file)
+         );
+ */
         setFiles(
           res.data.map((file: any) => ({
             name: file.name,
@@ -184,8 +210,6 @@ const FormSubmissions = () => {
   if (isLoading) {
     return <LoadingIndicatorPage />;
   }
-
-  console.log("submissionsData : ", submissionsData);
 
   return (
     <Layout>
@@ -212,7 +236,7 @@ const FormSubmissions = () => {
             footer={
               <Flex justifyContent="start" background="neutral150" padding={3}>
                 <Typography style={{ color: "gray" }}>
-                  {submissionsData?.submissions.length} &nbsp; Submissions
+                  {submissions?.submissions.length} &nbsp; Submissions
                 </Typography>
               </Flex>
             }
@@ -232,7 +256,7 @@ const FormSubmissions = () => {
               </Tooltip>
             }
           >
-            {submissionsData?.submissions.map((submission, idx) => (
+            {submissions?.submissions.map((submission, idx) => (
               <Accordion
                 expanded={expandedID === idx}
                 onToggle={handleToggle(idx)}
@@ -242,7 +266,8 @@ const FormSubmissions = () => {
               >
                 <AccordionToggle
                   startIcon={<User aria-hidden />}
-                  title={`User ${idx + 1}`}
+                  // use the index in reverse order to show the latest submission first
+                  title={`User ${submissions?.submissions.length - idx}`}
                   togglePosition="left"
                   action={
                     <AccordionAction submittedAt={submission.submittedAt} />
@@ -250,57 +275,109 @@ const FormSubmissions = () => {
                 />
                 <AccordionContent>
                   <Content>
-                    {Object.entries(submission.formData).map(([key, value]) => {
-                      /* console.log(
-                     "forms: ",
-                     submissionsData?.form.find(
-                       (form) => form.id == submission.formId
-                     )
-                   );*/
+                    {Object.entries(submission.formData).map(
+                      ([key, value], idx) => {
+                        let fileType;
+                        const isFileUpload = files.find(
+                          ({ name }) => name === value
+                        );
+                        if (isFileUpload) {
+                          fileType = getFileType(isFileUpload.mime);
+                        }
 
-                      let fileType;
-                      const isFileUpload = files.find(
-                        ({ name }) => name === value
-                      );
-                      if (isFileUpload) {
-                        fileType = getFileType(isFileUpload.mime);
+                        return (
+                          <Entry className="entries" key={idx}>
+                            <Key className="key">{key} : </Key> {"  "}
+                            {isFileUpload ? ( // open the link in a new tab
+                              // http://localhost:1337/uploads/default_image_ec2d9c4b17.png
+
+                              <LinkButton
+                                startIcon={<File />}
+                                target="_blank"
+                                href={`http://localhost:1337${isFileUpload.url}`}
+                                as="a"
+                                isExternal
+                              >
+                                {fileType === "image"
+                                  ? "Preview Image"
+                                  : "Download File"}
+                              </LinkButton>
+                            ) : (
+                              <Value className="value">{value || "-"}</Value>
+                            )}
+                          </Entry>
+                        );
                       }
-
-                      console.log("value : ", typeof value);
-
-                      return (
-                        <Entry className="entries">
-                          <Key className="key">{key} : </Key> {"  "}
-                          {isFileUpload ? ( // open the link in a new tab
-                            // http://localhost:1337/uploads/default_image_ec2d9c4b17.png
-
-                            <LinkButton
-                              startIcon={<File />}
-                              target="_blank"
-                              href={`http://localhost:1337${isFileUpload.url}`}
-                              as="a"
-                              isExternal
-                            >
-                              {fileType === "image"
-                                ? "Preview Image"
-                                : "Download File"}
-                            </LinkButton>
-                          ) : (
-                            <Value className="value">{value || "-"}</Value>
-                          )}
-                        </Entry>
-                      );
-                    })}
+                    )}
                   </Content>
                 </AccordionContent>
               </Accordion>
             ))}
           </AccordionGroup>
+          <PaginationWrapper>
+            <Pagination className="pagination">
+              <PreviousLink
+                as={NavLink}
+                to="/1"
+                disabled={true}
+                aria-disabled={page === 1}
+                onClick={(e: any) => {
+                  e.preventDefault();
 
-          <Pagination
-            activePage={1}
-            pageCount={submissionsData?.submissions.length}
-          ></Pagination>
+                  // set the page to previous page
+                  if (page === 1) return;
+                  setPage((page) => page - 1);
+                }}
+              >
+                Previous
+              </PreviousLink>
+              {pagination?.pageCount &&
+                Array.from(new Array(pagination.pageCount))
+                  .slice(0, 5)
+                  .map((page, idx) => (
+                    <PageNumber
+                      className={idx + 1 === pagination.page ? "active" : ""}
+                      key={idx}
+                      onClick={() => {
+                        if (idx + 1 == page) return;
+                        setPage(idx + 1);
+                      }}
+                    >
+                      {idx + 1}
+                    </PageNumber>
+                  ))}
+              {pagination?.pageCount && pagination?.pageCount > 5 && (
+                <React.Fragment>
+                  ...
+                  <PageNumber
+                    className={
+                      pagination?.page === pagination?.pageCount ? "active" : ""
+                    }
+                    onClick={() => {
+                      if (pagination?.page === pagination?.pageCount) return;
+                      setPage(pagination?.pageCount || 0);
+                    }}
+                  >
+                    {pagination?.pageCount && pagination.pageCount}
+                  </PageNumber>
+                </React.Fragment>
+              )}
+              <NextLink
+                as={NavLink}
+                to="/2"
+                aria-disabled={page === pagination?.pageCount}
+                onClick={(e: any) => {
+                  e.preventDefault();
+
+                  // set the page to the next page
+                  if (page === pagination?.pageCount) return;
+                  setPage((page) => page + 1);
+                }}
+              >
+                Next page
+              </NextLink>
+            </Pagination>
+          </PaginationWrapper>
         </Box>
       </ContentLayout>
     </Layout>
